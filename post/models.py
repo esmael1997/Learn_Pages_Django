3ebab0afc1a0ccc9d6e0357d26c1064a3faca0e8
Esmael <82362802+esmael1997@users.noreply.github.com>
2025-09-15 20:29:37 +0330

    add class detail for postblog and ad admin and update template

--- post/models.py
+++ post/models.py
@@ -1,8 +1,15 @@
 from django.db import models
-
+from django.urls import reverse
 
 class Post(models.Model):
     text = models.TextField()
+    auther = models.ForeignKey("auth.User", on_delete=models.CASCADE, null=False, blank=False)
+    title = models.CharField(max_length=255, default=" ")
     
     def __str__(self):
-        return self.text[:20]
+        return self.title[:20]
+    
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"pk": self.pk})
+    
+    
