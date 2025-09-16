81e91ba33b4c298cbd0928dc945f6de3e4f1ea92
Esmael <82362802+esmael1997@users.noreply.github.com>
2025-09-16 20:16:05 +0330

    add edit for post add html for edit

--- post/urls.py
+++ post/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import Blog, BlogDetailView, BlogCreateView
+from .views import Blog, BlogDetailView, BlogCreateView, BlogUpdateView
 
 
 
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', Blog.as_view(), name="blog"),
     path('<int:pk>/', BlogDetailView.as_view(), name='blog_detail'),
     path('new/', BlogCreateView.as_view(), name='blog_new'),
+    path('edit/', BlogUpdateView.as_view(), name="post_edit"),
 ]
--- post/views.py
+++ post/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView
+from django.urls import reverse_lazy
 from .models import Post
 
 class Blog(ListView):
@@ -18,3 +19,9 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = "post_new.html"
     fields = ["title", "author", "body"]
+    success_url = reverse_lazy('blog')
+    
+class BlogUpdateView(UpdateView): 
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]    
