d6573a7b509c46e6f7273e2742944ffe755db5ef
Esmael <82362802+esmael1997@users.noreply.github.com>
2025-09-17 20:55:49 +0330

    add form login,signup

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 from.views import SignUpView
+from . import views
+
 
 urlpatterns = [
-    path('signup/', SignUpView.as_view(), name="signup"),
+    #path('signup/', SignUpView.as_view(), name="signup"),
+    path('login/', views.login_view, name='login'),
+    path('signup/', views.signup_view, name='signup'),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,11 +1,39 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.contrib.auth import authenticate, login
+from .form import LoginForm
+from .forms import SignUpForm
+
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
 
-# Create your views here.
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('home')  
+    else:
+        form = LoginForm()
+    return render(request, 'accounts/login.html', {'form': form})
+
+def signup_view(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()       
+            login(request, user)     
+            return redirect('home')  
+    else:
+        form = SignUpForm()
+    return render(request, 'accounts/signup.html', {'form': form})
